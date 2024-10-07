c9970359a9ad2e02d68bfe8a8dc7dc1739b4de7f
pokuwagata <pokuwagata@gmail.com>
2024-10-07 16:49:56 +0900

    init

---
+++ src/pages/contents50.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+interface Data {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export default function Contents20() {
+  return new Array(50).fill(1).map((_, index) => {
+    return <Contents key={index} id={index + 1} />;
+  });
+}
+
+function Contents(props: { id: number }) {
+  const [data, setData] = useState<Data | null>(null);
+  useEffect(() => {
+    let ignore = false;
+    setData(null);
+    (async () => {
+      await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${props.id}`
+      ).then(async (result) => {
+        if (!ignore) {
+          setData(await result.json());
+        }
+      });
+    })();
+    return () => {
+      ignore = true;
+    };
+  }, [props.id]);
+
+  if (data === null) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <img
+        src={`https://placehold.jp/3d4070/ffffff/600x336.png?text=${props.id}`}
+      />
+      <p>{data.title}</p>
+      <p>{data.body}</p>
+    </>
+  );
+}
